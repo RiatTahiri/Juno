48a33fb393af5920d2c080f4384a090d3a3d69a5
Kloops Games <badvidgamer@gmail.com>
2024-03-31 01:41:38 +0100

    update

--- src/App.java
+++ src/App.java
@@ -2,9 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        int test1 = 10;
-        int test2 = 20;
-
-        System.out.println(test1 + test2);
     }
 }
