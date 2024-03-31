c2c14029541f08f1c5872eb07f12d859acc7ff52
Kloops Games <badvidgamer@gmail.com>
2024-03-31 01:16:27 +0100

    Intepreter Creation -Juno-

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int test1 = 10;
+        int test2 = 20;
+
+        System.out.println(test1 + test2);
+    }
+}
